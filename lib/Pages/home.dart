2d6ac5329619eaf57cc12c3ad45efafa37373e5d
Naresh kumar <nareshpt253@gmail.com>
2024-04-05 00:08:01 +0530

    LogOut Added

--- lib/Pages/home.dart
+++ lib/Pages/home.dart
@@ -1,4 +1,6 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_app/Pages/login.dart';
 
 class homePage extends StatefulWidget {
   const homePage({super.key});
@@ -14,6 +16,19 @@ class _homePageState extends State<homePage> {
       appBar: AppBar(
         title: Text("Home Page"),
         centerTitle: true,
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          ElevatedButton(
+              onPressed: () {
+                FirebaseAuth.instance.signOut().then((value) {
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => loginPage()));
+                });
+              },
+              child: Text("LogOut"))
+        ],
       ),
     );
   }
