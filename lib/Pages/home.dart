5b7fd82f14fd7cd4776e78d56e273973be15895d
Naresh kumar <nareshpt253@gmail.com>
2024-04-05 00:14:48 +0530

    LogOut Added

--- lib/Pages/home.dart
+++ lib/Pages/home.dart
@@ -13,22 +13,20 @@ class _homePageState extends State<homePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      floatingActionButtonLocation: FloatingActionButtonLocation.endDocked,
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.logout),
+        backgroundColor: Colors.green,
+        onPressed: () {
+          FirebaseAuth.instance.signOut().then((value) {
+            Navigator.push(
+                context, MaterialPageRoute(builder: (context) => loginPage()));
+          });
+        },
+      ),
       appBar: AppBar(
         title: Text("Home Page"),
         centerTitle: true,
-      ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          ElevatedButton(
-              onPressed: () {
-                FirebaseAuth.instance.signOut().then((value) {
-                  Navigator.push(context,
-                      MaterialPageRoute(builder: (context) => loginPage()));
-                });
-              },
-              child: Text("LogOut"))
-        ],
       ),
     );
   }
